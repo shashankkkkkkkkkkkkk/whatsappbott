ae2988907c00bb0502970f42fdb3cb699d6ed662
Shashank <shashankthamali2099@gmail.com>
2026-02-20 20:54:59 +0530

    test deployment

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
 const express = require("express");
 
 const app = express();
-app.use(express.json());
-
 const PORT = process.env.PORT || 3000;
 
-// Root route
 app.get("/", (req, res) => {
-  res.send("Server is alive");
+  res.send("NEW CODE WORKING");
 });
 
-// Webhook verification
 app.get("/webhook", (req, res) => {
-  const mode = req.query["hub.mode"];
-  const token = req.query["hub.verify_token"];
-  const challenge = req.query["hub.challenge"];
-
-  if (mode === "subscribe" && token === "desilife123") {
-    return res.status(200).send(challenge);
-  } else {
-    return res.sendStatus(403);
-  }
+  res.send("WEBHOOK HIT");
 });
 
 app.listen(PORT, () => {
