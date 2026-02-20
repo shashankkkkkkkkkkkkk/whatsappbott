84fd04196f236a1805c8fcf74bfe314a104170cb
Shashank <shashankthamali2099@gmail.com>
2026-02-20 20:56:42 +0530

    clean webhook test

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Root route
 app.get("/", (req, res) => {
-  res.send("NEW CODE WORKING");
+  res.send("Server working");
 });
 
+// Webhook verification (hardcoded token for now)
 app.get("/webhook", (req, res) => {
-  res.send("WEBHOOK HIT");
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === "desilife123") {
+    return res.status(200).send(challenge);
+  } else {
+    return res.sendStatus(403);
+  }
 });
 
 app.listen(PORT, () => {
